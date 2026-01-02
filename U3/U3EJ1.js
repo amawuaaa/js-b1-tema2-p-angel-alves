4a2aee1dee92ab5ac32a386fcd4ff5d19402b8af
amawuaaa <angelurquiola212@hotmail.com>
2026-01-02 21:47:05 +0000

    U3EJ1 completado

--- U3/U3EJ1.js
+++ U3/U3EJ1.js
@@ -34,6 +34,25 @@ class Car {
 //Escribe aquí tu solución / escriviu aquí la vostra solució:
 
 
+class Car {
+  constructor(brand, model, displacement, horsePower, year) {
+    this.brand = brand;
+    this.model = model;
+    this.displacement = displacement;
+    this.horsePower = horsePower;
+    this.year = year;
+  }
+
+  // SETTER
+  set carAntiguaty(yearsOld) {
+    this.year = 2023 - yearsOld;
+  }
+
+  // GETTER
+  get getCarDescription() {
+    return `${this.brand} ${this.model} ${this.displacement}`+"cc";
+  }
+}
 
 
 
